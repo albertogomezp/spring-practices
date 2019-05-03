12bcdc60f176f7f981573a602a76e52e5a561256
Alberto <proyectos@albertogomp.es>
2019-05-03 13:44:47 +0200

    varios

---
+++ Hola-Mundo-thymeleaf/src/main/java/com/example/demo/holamundo/HolaMundoThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.holamundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundoThymeleafApplication.class, args);
+	}
+
+}
--- Hola-Mundo/src/main/java/com/example/demo/app/IndexController.java
+++ Hola-Mundo/src/main/java/com/example/demo/app/IndexController.java
@@ -1,20 +1,26 @@
 package com.example.demo.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-
+		
+	@Value("${application.controller.titulo}")
+	private String titulo;
+	
+	
 		/**
 		 * Devuelve hola mundo
+		 * RequestMapping para GET y POST
 		 * @param model
 		 * @return
 		 */
-		@GetMapping("/index") //RequestMapping para GET y POST
+		@GetMapping("/index") 
 		public String index(Model model) {
-			model.addAttribute("titulo","Hola mundo");
+			model.addAttribute("titulo",titulo);
 			return "index";
 		}
 }
