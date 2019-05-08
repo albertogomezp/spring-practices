525e7328c37d06f3e52511d435beb6a8f3c53737
Alberto <proyectos@albertogomp.es>
2019-05-08 11:25:40 +0200

    Update

---
+++ spring-boot-di/src/main/java/com/thana/sprinboot/app/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.thana.sprinboot.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.thana.sprinboot.app.model.service.MiServicio;
+
+@Controller
+public class IndexController {
+	
+	@Autowired
+	private MiServicio servicio;
+	
+	@GetMapping({"/","","/index"})
+	public String index(Model model) {
+		model.addAttribute("objeto", servicio.operacion());
+
+		return "index";
+	}
+}
---
+++ spring-boot-di/src/main/java/com/thana/sprinboot/app/model/service/MiServicio.java
@@ -0,0 +1,12 @@
+package com.thana.sprinboot.app.model.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MiServicio {
+
+	
+	public String operacion() {
+		return "Hackeando el petagono... Iniciando HTML....";
+	}
+}
