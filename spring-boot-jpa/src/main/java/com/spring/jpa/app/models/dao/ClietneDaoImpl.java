4092ff3b80381f18f2eae5a71e46d4f2853a7d3b
Alberto <proyectos@albertogomp.es>
2019-05-10 08:34:03 +0200

    Auto stash before merge of "master" and "origin/master"

--- spring-boot-jpa/src/main/java/com/spring/jpa/app/models/dao/ClietneDaoImpl.java
+++ spring-boot-jpa/src/main/java/com/spring/jpa/app/models/dao/ClietneDaoImpl.java
@@ -2,14 +2,27 @@ package com.spring.jpa.app.models.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.spring.jpa.app.models.entity.Cliente;
 
-public class ClietneDaoImpl implements IClienteDao {
 
+@Repository
+public class ClietneDaoImpl implements IClienteDao {
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	@SuppressWarnings("unchecked")
+	@Transactional(readOnly=true)
 	@Override
 	public List<Cliente> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return em.createQuery("from CLiente").getResultList();
 	}
 
 }
