728d3b6e4e448cecd0b95fd160701193286eeec8
Alberto <proyectos@albertogomp.es>
2019-05-16 13:39:28 +0200

    Update

--- sping-boot-data-jpa3/src/main/java/com/spring/jpa/app/controllers/ClienteController.java
+++ sping-boot-data-jpa3/src/main/java/com/spring/jpa/app/controllers/ClienteController.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.jpa.app.models.entity.Cliente;
 import com.spring.jpa.app.models.service.IClienteService;
@@ -64,8 +64,7 @@ public class ClienteController {
 	 * @return
 	 */
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
-	public String guardar(@Valid @ModelAttribute("cliente") Cliente cliente, BindingResult result, Model model,
-			SessionStatus status) {
+	public String guardar(@Valid @ModelAttribute("cliente") Cliente cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status) {
 		if (result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario de Cliente");
 			model.addAttribute("accion", "Intentelo de nuevo");
@@ -75,6 +74,8 @@ public class ClienteController {
 		} else {
 			clienteService.save(cliente);
 			status.setComplete();
+			flash.addFlashAttribute("msg","true");
+			flash.addFlashAttribute("success", "Cliente guardado con éxito");
 			return "redirect:listar";
 		}
 
@@ -86,25 +87,34 @@ public class ClienteController {
 	 * @return
 	 */
 	@RequestMapping(value = "/form/{id}")
-	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model) {
+	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash ) {
 		Cliente cliente = null;
 		if (id > 0) {
 			cliente = clienteService.findOne(id);
+			if (cliente == null) {
+				flash.addFlashAttribute("error", "El ID del cliente no existe en la base de datos");
+
+			}
 		} else {
+			flash.addAttribute("error", "El ID del cliente no puede ser 0");
+
 			return "redirect:/listar";
 		}
 		model.put("cliente", cliente);
 		model.put("titulo", "Editar ".concat(cliente.getNombre().concat(" ").concat(cliente.getApellido())));
 		model.put("accion", "Editar cliente");
 		model.put("whatis", "true");
-
+	
 		return "form";
 
 	}
 	@RequestMapping(value="/eliminar/{id}")
-	public String eliminar(@PathVariable(value = "id") Long id) {
+	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
 		if(id>0) {
 			clienteService.delete(id);
+			flash.addFlashAttribute("msg","true");
+			flash.addFlashAttribute("success", "Cliente eliminado con éxito");
+
 		}
 		return "redirect:/listar";
 	}
