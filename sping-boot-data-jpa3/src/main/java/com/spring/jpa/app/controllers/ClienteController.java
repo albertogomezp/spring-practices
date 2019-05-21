12294e70af2a7ff53ad52ee588010e0f068f7c5e
Alberto <proyectos@albertogomp.es>
2019-05-21 13:15:43 +0200

    update

--- sping-boot-data-jpa3/src/main/java/com/spring/jpa/app/controllers/ClienteController.java
+++ sping-boot-data-jpa3/src/main/java/com/spring/jpa/app/controllers/ClienteController.java
@@ -7,7 +7,6 @@ import java.nio.file.Paths;
 import java.util.Map;
 
 import javax.validation.Valid;
-import javax.websocket.Session;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,6 +37,20 @@ public class ClienteController {
 	@Autowired // esto encontrará el repo
 //	@Qualifier("IClienteDaoJPA") // Para mapear en caso de ambiguedad
 	private IClienteService clienteService;
+	
+	@GetMapping(value="/ver/{id}") 
+	public String ver(@PathVariable(value="id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+		
+		Cliente cliente = clienteService.findOne(id);
+		if(cliente ==null) {
+			flash.addFlashAttribute("msg","true");
+			flash.addFlashAttribute("info", "El cliente no existe en la base de datos");
+			return "redirect:/listar";
+		}
+		model.put("cliente", cliente);
+		model.put("titulo", "Detalles del cliente: "+cliente.getNombre());
+		return "/ver";
+	}
 	
 	/**
 	 * Lista todos los usuarios (por GET)
@@ -100,8 +114,8 @@ public class ClienteController {
 			return "form";
 		} else {
 			if(!foto.isEmpty()) {
-				Path directorioRecursos = Paths.get("src//main//resources//static/uploads");
-				String rootPath = directorioRecursos.toFile().getAbsolutePath();
+				//Path directorioRecursos = Paths.get("src//main//resources//static/uploads");
+				String rootPath = "C://Temp//uploads";
 				try {
 					byte[] bytes = foto.getBytes();
 					Path rutaCompleta = Paths.get(rootPath+ "//" + foto.getOriginalFilename());
@@ -110,7 +124,6 @@ public class ClienteController {
 					flash.addFlashAttribute("msg","true");
 					cliente.setFoto(foto.getOriginalFilename());
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
