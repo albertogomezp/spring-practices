66e7aff831b5f631153ac55233ab55830679356d
Alberto <proyectos@albertogomp.es>
2019-05-21 13:26:30 +0200

    Update
    Fixed minor bug

--- sping-boot-data-jpa3/src/main/java/com/spring/jpa/app/MvcConfig.java
+++ sping-boot-data-jpa3/src/main/java/com/spring/jpa/app/MvcConfig.java
@@ -12,7 +12,7 @@ public class MvcConfig implements WebMvcConfigurer{
 		
 		WebMvcConfigurer.super.addResourceHandlers(registry);
 		registry.addResourceHandler("/uploads/**")
-		.addResourceLocations("file:/C:/Temp/uploads");
+		.addResourceLocations("file:/C:/Temp/uploads/");
 	}
 
 }
